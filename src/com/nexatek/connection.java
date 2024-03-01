bd52f41b2eb422c7259d65c85fb281f4cbf08b78
ENG-MARTIN <maartinsseba3@gmail.com>
2024-03-01 15:20:10 +0300

    Employee window

--- src/com/nexatek/connection.java
+++ src/com/nexatek/connection.java
@@ -14,7 +14,7 @@ public class connection {
     public static Connection connect() {
         String jdbcURL = "jdbc:postgresql://localhost:5432/luckyelectronicals";
         String username = "postgres";
-        String password = "planet";
+        String password = "cp12345";
 
         try {
             Connection connection = DriverManager.getConnection(jdbcURL, username, password);
